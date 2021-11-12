751afd36fb99e4af99ee171a1e9f4706e6449470
mohdmehdi2021 <81911929+mohdmehdi2021@users.noreply.github.com>
2021-11-12 20:05:10 +0530

    Simple Music App
    App that run on console. A small project.

--- Main.java
+++ Main.java
@@ -1,24 +1,107 @@
-package com.anoman.javainterm;
+// *************************************************** Project Name = Music App ***************************************************
+// *************************************************** Developer Name = Mohd Mehdi khan *******************************************
+// *************************************************** Development started = 12/11/2021 *******************************************
 
-import java.net.*;
 import java.util.Scanner;
 
-public class Main {
+public class Main{
+ public static void main(String args[]){
 
-    public static void main(String[] args) throws UnknownHostException {
-	// write your code here
-        // IP4 address Detector
-        String website;
-        Scanner input;
-        input= new Scanner(System.in);
+int language_option,hindiSongOption;
 
-        System.out.println("Enter the website domain as, example.com");
-        website = input.nextLine();
+Scanner input = new Scanner(System.in); // ***********************SCANNER
 
-        InetAddress address_of_target;
-        address_of_target = InetAddress.getByName(website);
-        System.out.println("Target Website : "+website);
-        System.out.println("\t IP version 4 : "+address_of_target);
+System.out.println("\n\tMusic App v1.0");
+System.out.println("\tDeveloped by = Mohd Mehdi Khan");
+System.out.println("\tDevelopment Date = 12/11/2021\n\n\n");
 
-    }
+System.out.println("\tWelcome To Music App");
+System.out.println("\tPlease choose the language : \n\t1 for Hindi \n\t2 for English \n\t3 for Urdu");
+
+
+language_option = input.nextInt();
+
+
+if(language_option == 1){
+	System.out.println("\tChoose index number to play that song.");
+	System.out.println("\t1. Ae dil hai mushkil");
+	System.out.println("\t2. Ya Ali ");
+	System.out.println("\t3. Maa tujhe salam");
+	
+	hindiSongOption = input.nextInt();
+	
+	if(hindiSongOption == 1){
+		System.out.println("\tPlaying Ae dil hai mushkil... ... ...");
+		System.out.println("\t----Hope you Enjoyed!----");
+	}
+	else if(hindiSongOption == 2){
+		System.out.println("\tPlaying Ya Ali... ... ...");
+		System.out.println("\t----Hope you Enjoyed!----");
+	}
+	else if(hindiSongOption == 3){
+		System.out.println("\tPlaying Maa Tujhe Salam... ... ...");
+		System.out.println("\t----Hope you Enjoyed!----");
+	}
+	else{
+		System.out.println("\tUnexpected choice. Closing the app.");
+	}
+
+
+}
+else if(language_option == 2){
+	System.out.println("\tChoose index number to play that song.");
+	System.out.println("\t1. Shape Of You");
+	System.out.println("\t2. Severus Snape Song ");
+	System.out.println("\t3. Twinkle Twinkle Little Star");
+	
+	hindiSongOption = input.nextInt();
+	
+	if(hindiSongOption == 1){
+		System.out.println("\tPlaying Shape Of You... ... ...");
+		System.out.println("\t----Hope you Enjoyed!----");
+	}
+	else if(hindiSongOption == 2){
+		System.out.println("\tPlaying Severus Snape Song... ... ...");
+		System.out.println("\t----Hope you Enjoyed!----");
+	}
+	else if(hindiSongOption == 3){
+		System.out.println("\tPlaying Twinkle Twinkle Little Star... ... ...");
+		System.out.println("\t----Hope you Enjoyed!----");
+	}
+	else{
+		System.out.println("\tUnexpected choice. Closing the app.");
+	}
+
+}
+else if(language_option == 3){
+	System.out.println("\tChoose index number to play that song.");
+	System.out.println("\t1. Idhar Zindagi ka Janaza");
+	System.out.println("\t2. Main Abdul Qadir Hoon ");
+	System.out.println("\t3. Mujhe Dushman ke bachcho ko padhana hai");
+	
+	hindiSongOption = input.nextInt();
+	
+	if(hindiSongOption == 1){
+		System.out.println("\tPlaying Idhar Zindagi ka Janaza... ... ...");
+		System.out.println("\t----Hope you Enjoyed!----");
+	}
+	else if(hindiSongOption == 2){
+		System.out.println("\tPlaying Main Abdul Qadir Hoon... ... ...");
+		System.out.println("\t----Hope you Enjoyed!----");
+	}
+	else if(hindiSongOption == 3){
+		System.out.println("\tPlaying Mujhe Dushman ke bachcho ko padhana hai... ... ...");
+		System.out.println("\t----Hope you Enjoyed!----");
+	}
+	else{
+		System.out.println("\tUnexpected choice. Closing the app.");
+	}
+}
+else{
+	System.out.println("Unexpected Choice. Closing the app.");
+}
+
+
+
+}
 }
