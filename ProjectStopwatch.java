4831f64e67ef0d4777fff8681de19d07ef1aeed3
mohdmehdi2021 <81911929+mohdmehdi2021@users.noreply.github.com>
2022-05-02 13:48:10 +0530

    StopWatch
    Developer = mohdmehdi2021

---
+++ ProjectStopwatch.java
@@ -0,0 +1,27 @@
+// stopwatch code = MohdMehdi2021
+
+class Stopwatch{
+    public void startwatch() throws Exception{
+
+
+        while (true) {
+            for (int hh = 0;; hh++) {
+                for (int mm = 0; mm <= 59; mm++) {
+                    for (int ss = 0; ss <= 59; ss++) {
+                        System.out.print("\r");
+                        System.out.print(hh+" : "+mm+" : "+ss);
+                        Thread.sleep(1000);
+                    }
+                }
+            }
+        }
+
+    }
+}
+public class ProjectStopwatch {
+    public static void main(String[] args) throws Exception {
+       Stopwatch stopwatch = new Stopwatch();
+       stopwatch.startwatch();
+        // 02 : 22 : 23 = format
+    }
+}
