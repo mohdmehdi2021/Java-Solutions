23c55ad7377723c0d11215a6c299c7b7d65d0536
mohdmehdi2021 <81911929+mohdmehdi2021@users.noreply.github.com>
2022-05-02 13:37:29 +0530

    Loading Progress Bar ------->

--- Main.java
+++ Main.java
@@ -1,27 +1,10 @@
-package com.mohdmehdi.ccjava;
+// coder = Mohd Mehdi s/o Arshad Abbas Khan (mohdmehdi2021)
 
-class Stopwatch{
-    public void startwatch() throws Exception{
-
-
-        while (true) {
-            for (int hh = 0;; hh++) {
-                for (int mm = 0; mm <= 59; mm++) {
-                    for (int ss = 0; ss <= 59; ss++) {
-                        System.out.print("\r");
-                        System.out.print(hh+" : "+mm+" : "+ss);
-                        Thread.sleep(1000);
-                    }
-                }
-            }
-        }
-
-    }
-}
 public class Main {
     public static void main(String[] args) throws Exception {
-       Stopwatch stopwatch = new Stopwatch();
-       stopwatch.startwatch();
-        // 02 : 22 : 23
+        for (int c = 1; c <= 100; c++) {
+            System.out.format("\b->",c);
+            Thread.sleep(350);
+        }
     }
 }
